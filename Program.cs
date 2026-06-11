2da5dc916889ab5c063f3c981d3dabb766cf70e7
Nicolly Barboza <nicolly_barboza@estudante.sesisenai.org.br>
2026-06-10 22:13:48 -0300

    contador

--- Program.cs
+++ Program.cs
@@ -1,14 +1,15 @@
-﻿// Escreva a mesnsagem "informe um numero: "
-Console.Write("informe um numero: ");
-//Cria a variabelQueSalvaUmNumero,
-//leia o que o usuário digitar
-// e tranforma o valor lido de texto para o inteiro (int.Parse)
-int variavelQueSalvaUmNumero = int.Parse(Console.ReadLine()!);
+﻿using System;
 
-//verifica se o valor de variavelQueSalvaUmNumero é par
-if(variavelQueSalvaUmNumero % 2 ==0) {
-    Console.WriteLine($"Número {variavelQueSalvaUmNumero} é par");
-}   else {Console.WriteLine($"Número {variavelQueSalvaUmNumero} é ímpar"); }
+class Program
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
 
 
 
