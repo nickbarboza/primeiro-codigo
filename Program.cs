ab35fa1594579187f9b82898e533a4323eec3f82
Nicolly Barboza <nicolly_barboza@estudante.sesisenai.org.br>
2026-06-10 21:51:18 -0300

    par e ímpar

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Escreva a mesnsagem "informe um numero: "
+Console.Write("informe um numero: ");
+//Cria a variabelQueSalvaUmNumero,
+//leia o que o usuário digitar
+// e tranforma o valor lido de texto para o inteiro (int.Parse)
+int variavelQueSalvaUmNumero = int.Parse(Console.ReadLine()!);
+
+//verifica se o valor de variavelQueSalvaUmNumero é par
+if(variavelQueSalvaUmNumero % 2 ==0) {
+    Console.WriteLine($"Número {variavelQueSalvaUmNumero} é par");
+}   else {Console.WriteLine($"Número {variavelQueSalvaUmNumero} é ímpar"); }
+
+
+
+    
+   
